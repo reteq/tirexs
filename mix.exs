d2e642e2f390c90c824bc17c91e4fc2491b87cb2
Genki Zhang <genkiwow@gmail.com>
2015-07-09 19:26:27 +0800

    start inets when server starts

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tirexs.Mixfile do
     [ app: :tirexs, version: "0.7.0", elixir: "~> 1.0.0", description: description, package: package, deps: deps ]
   end
 
-  def application, do: []
+  def application, do: [:exjsx, :inets]
 
   defp deps do
     [ {:exjsx, github: "talentdeficit/exjsx", tag: "v3.1.0"} ]
